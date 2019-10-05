7e97f3d05d5b4a985ac7bd713c9c58a89fff9043
Adit Alware <42518974+aditalware@users.noreply.github.com>
2019-10-05 14:04:05 +0530

    Create buttonclick.js

---
+++ src/js/buttonclick.js
@@ -0,0 +1,15 @@
+function Buttons()
+{ 
+var b1=document.getElementById("name").value;
+var b2 = document.getElementById("age").value;
+var b3=document.getElementById("pass").value;
+var b4 = document.getElementById("pass2").value;
+var b5=document.getElementById("cont").value;
+var b6 = document.getElementById("aad").value;
+if(b1=="")
+{
+  alert("Name Required");
+}
+else if(b2.checked==true)
+alert("option2");
+}
