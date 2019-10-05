cb3909ea0d5cee1cc4201becd76e05a3f4cdeac8
Harish Kumar <201851046@iiitvadodara.ac.in>
2019-10-05 22:09:28 +0530

    Create check.js

---
+++ src/js/check.js
@@ -0,0 +1,19 @@
+function validate()
+{
+var user=document.getElementById("h");
+var pass=document.getElementById("u");
+if(user.value=="-None-")
+{
+    alert("form should not be empty!");
+   
+}
+else 
+{
+ if(h.value=="Majra Chaukdi")   
+{
+    alert("hello!");
+var viewer= document.createElement("img");
+viewer.src="as3.jpg";
+}
+}
+}
